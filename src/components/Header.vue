b6796b985038d37106b7a246258b800cbddc76c3
KaiaWalters <kaia.cwalters@gmail.com>
2021-02-21 09:06:14 -0500

    Wip change headers media queries

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,7 +14,7 @@ export default {
 @import "/src/assets/css/noscript.css";
 
 #header {
-  height:70vh;
+  height:75vh;
   width:100%;
   background-image: url('../assets/css/images/home-title-card.png');
   -webkit-background-size: contain;
@@ -25,52 +25,91 @@ export default {
   background-attachment: fixed;
   background-position: 0px 15px;
 }
-@media screen and (max-width: 900px) {
-  #header {
-    height:44vh;
-    width:100%;
-    background-image: url('../assets/css/images/mobiletitlecard.svg');
-    -webkit-background-size: contain;
-    -moz-background-size: contain;
-    -o-background-size: cover;
-    background-size:contain;
-    background-repeat: no-repeat;
-    background-position: 0px 90px;
-  }
-}
 
-@media (min-width: 411px) and (max-width: 980px) {
-  #header {
-    background-position: 0px 100px;
-    height:25vh;
-  }
-}
 /* dont touch me!!!! */
-@media  screen and (max-width: 378px) {
+/* @media  screen and (max-width: 378px) {
   #header {
     background-position: 0px 100px;
     height:28.5vh;
   }
-}
-
+} */
+/* 
 @media (max-width:768px) {
    #header {
     background-position: 0px 100px;
     height:37vh;
   }
-}
+} */
+/* 360 */
 
-@media  (max-width: 375px) {
+@media screen and (max-width: 320px) {
   #header {
-    background-position: 0px 100px;
-    height:22.75vh;
+     background-image: url('../assets/css/images/mobiletitlecard.svg');
+     background-position: 0px 125px;
+     height: 50vh;
   }
 }
 
-@media (min-width: 901px) and (max-width: 1024px) {
+@media  (min-width: 325px) and (max-width: 375px) {
   #header {
+     background-image: url('../assets/css/images/mobiletitlecard.svg');
+     background-position: 0px 115px;
+     height: 40vh;
+  }
+}
+
+@media  (min-width: 410px) and (max-width: 630px) {
+  #header {
+     background-image: url('../assets/css/images/mobiletitlecard.svg');
     background-position: 0px 100px;
-    height:30.5vh;
+    height:41.75vh;
+  }
+}
+
+@media (min-width: 630px) and (max-width: 725px) {
+  #header {
+    background-image: url('../assets/css/images/mobiletitlecard.svg');
+    background-position: 0px 100px;
+    height:45vh;
+  }
+}
+
+@media (min-width: 725px) and (max-width: 760px) {
+  #header {
+    background-image: url('../assets/css/images/mobiletitlecard.svg');
+    background-position: 0px 100px;
+    height:65vh;
+  }
+}
+
+@media (min-width: 761px) and (max-width: 768px) {
+  #header {
+    background-image: url('../assets/css/images/mobiletitlecard.svg');
+    background-position: 0px 100px;
+    height:50vh;
+  }
+}
+
+@media (min-width: 771px) and (max-width: 780px) {
+  #header {
+    background-image: url('../assets/css/images/mobiletitlecard.svg');
+    background-position: 0px 100px;
+    height:77vh;
+  }
+}
+
+@media (min-width: 901px) and (max-width: 1059px) {
+  #header {
+    background-image: url('../assets/css/images/home-title-card.png');
+    background-position: 0px 100px;
+    height:40vh !important;
+  }
+}
+
+/* mobile */
+@media screen and (max-width: 900px) {
+  #header {
+     background-image: url('../assets/css/images/mobiletitlecard.svg') !important;
   }
 }
 </style>
