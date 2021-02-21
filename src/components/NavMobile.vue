21de79447fee9b2de3daa7f86d317ffe6d3d6a18
KaiaWalters <kaia.cwalters@gmail.com>
2021-02-20 22:58:06 -0500

    Add social media icons to nav bar

--- src/components/NavMobile.vue
+++ src/components/NavMobile.vue
@@ -6,6 +6,32 @@
       <router-link @click="toggleNavMenu()" :to="{name:'Home'}">MEET NIKKIA</router-link>
       <router-link @click="toggleNavMenu()" :to="{name:'Issues'}">ISSUES</router-link>
       <a href="https://docs.google.com/forms/d/e/1FAIpQLSdbAr151anh7MYOuYqnFH14b3n9S1H7NpVzTN-EMiLKjrJcGQ/viewform?usp=pp_url">JOIN US</a>
+      <ul class="icons">
+        <li>
+          <a
+            href="https://twitter.com/Nikkia4Boston"
+            class="icon brands alt fa-twitter"
+            ><span class="label">Twitter</span></a
+          >
+        </li>
+        <!-- <li>
+          <a
+            href="https://www.instagram.com/nikkia4boston/"
+            class="icon brands alt fa-facebook-f"
+            ><span class="label">Facebook</span></a
+          >
+        </li> -->
+        <li>
+          <a href="https://www.instagram.com/nikkia4boston/" class="icon brands alt fa-instagram"
+            ><span class="label">Instagram</span></a
+          >
+        </li>
+        <li>
+          <a href="mailto:nikkiajeancharles@gmail.com" class="icon solid alt fa-envelope"
+            ><span class="label">Email</span></a
+          >
+        </li>
+      </ul>
       <a class="highlight" href="https://secure.actblue.com/donate/nikkia4boston">DONATE</a>
     </ul>
   </div>
@@ -28,6 +54,10 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
+  .icons {
+    padding: 0px !important;
+  }
 
   .hideMenu {
     display:none; 
